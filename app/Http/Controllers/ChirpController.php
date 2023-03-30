4a5e98f2246663665231024d5eedd926b87cf4a5
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-03-30 10:27:35 -0300

    Add and limit chirp length using controller method

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -31,6 +32,13 @@ class ChirpController extends Controller
     public function store(Request $request)
     {
         //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $request->user()->chirps()->create($validated);
+ 
+        return redirect(route('chirps.index'));
     }
 
     /**
